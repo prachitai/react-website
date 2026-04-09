b6eb7cd78377460be10d1ff910b05a183ec7c888
prachitai <192980789+prachitai@users.noreply.github.com>
2026-04-09 22:24:55 +0530

    final working version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Weather from "./pages/Weather";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/weather" element={<Weather />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
