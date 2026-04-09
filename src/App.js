0a33dd0bb2fd684cb90e4a7351f92975ffa8fbdb
prachitai <192980789+prachitai@users.noreply.github.com>
2026-04-09 23:15:26 +0530

    fix all pages minimal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
