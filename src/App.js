a893deb4a1becb62ac1b363962f9e18ca8a89d5e
prachitai <192980789+prachitai@users.noreply.github.com>
2026-04-09 22:17:42 +0530

    test isolate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Education from "./pages/Education";
-import Skills from "./pages/Skills";
-import Weather from "./pages/Weather";
-
 function App() {
-  return (
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/weather" element={<Weather />} />
-      </Routes>
-
-      <Footer />
-    </Router>
-  );
+  return <h1>FINAL TEST WORKING</h1>;
 }
 
 export default App;
