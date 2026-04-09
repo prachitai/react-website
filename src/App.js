236c610e2568895dd9cf1a7701fb34670b89f33b
prachitai <192980789+prachitai@users.noreply.github.com>
2026-04-09 22:57:59 +0530

    final working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Education from "./pages/Education";
-import Skills from "./pages/Skills";
-import Weather from "./pages/Weather";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-
-      <Home />
-      <Contact />
-      <Education />
-      <Skills />
-      <Weather />
-
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
